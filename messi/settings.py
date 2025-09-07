2eb422f843c8f98bc9561101acf342e5b239afb7
unknown <ifadhil63@gmail.com>
2025-09-07 16:51:18 +0700

    changed the host

--- messi/settings.py
+++ messi/settings.py
@@ -31,7 +31,7 @@ PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "152.118.29.139", "fadhil-daffa-Pulga10Store.pbp.cs.ui.ac.id"]
+ALLOWED_HOSTS = ["localhost", "152.118.29.139", "fadhil-daffa-footballshop.pbp.cs.ui.ac.id"]
 
 
 # Application definition
